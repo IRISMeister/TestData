d8bf5ef7bd62fa9818f709d6dc6b714b45d98cf3
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2019-10-02 18:02:57 +0900

    add default file encoding to java

--- runCSV.bat
+++ runCSV.bat
@@ -1,2 +1,2 @@
 set IRISJAR=C:\InterSystems\IRIS20191\dev\java\lib\JDK18
-java -cp %IRISJAR%/intersystems-utils-3.0.0.jar;%IRISJAR%/intersystems-jdbc-3.0.0.jar;%IRISJAR%/intersystems-xep-3.0.0.jar com.intersystems.datatransfer.SimpleMover p=%1
+java -Dfile.encoding=UTF-8 -cp %IRISJAR%/intersystems-utils-3.0.0.jar;%IRISJAR%/intersystems-jdbc-3.0.0.jar;%IRISJAR%/intersystems-xep-3.0.0.jar com.intersystems.datatransfer.SimpleMover p=%1
